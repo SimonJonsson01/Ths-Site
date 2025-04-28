bcd26f0474b05759405eb87827f41fecc4a58281
SimonJonsson01 <simonjonsson01@hotmail.com>
2025-04-28 10:45:24 +0200

    Initial commit

---
+++ ths-site-backend/src/main/java/ths_site/backend/ThsSiteBackendApplication.java
@@ -0,0 +1,13 @@
+package ths_site.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThsSiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThsSiteBackendApplication.class, args);
+	}
+
+}
---
+++ ths-site-backend/src/test/java/ths_site/backend/ThsSiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ths_site.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThsSiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
