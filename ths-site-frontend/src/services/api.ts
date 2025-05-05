d4ba8ad645de4ca8205949a1cba5b0a7e9375073
Simon J <simonjonsson01@hotmail.com>
2025-05-05 15:44:33 +0200

    added Dockerfiles, Docker Compose and other changes

--- ths-site-frontend/src/services/api.ts
+++ ths-site-frontend/src/services/api.ts
@@ -1,4 +1,4 @@
 /*
  * THS_SITE_API_URL kommer att vara URL för koppling till backend (Spring Boot).
+ * const THS_SITE_API_URL: string = import.meta.env.THS_SITE_API_URL;
  */
-const THS_SITE_API_URL: string = import.meta.env.THS_SITE_API_URL;
